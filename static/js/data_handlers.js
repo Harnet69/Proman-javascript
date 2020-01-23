d9bf9854176a796347b6cf2867570146eb2f36bd
Harnet69 <harnet69@gmail.com>
2020-01-23 18:24:10 +0100

    Changed the format of a saved board to Obj

--- static/js/data_handlers.js
+++ static/js/data_handlers.js
@@ -1,14 +1,24 @@
 import {clearBoard} from "./dom_manipulations.js"
 // save data from board to array
 export function getDataFromBoard() {
-    let boardData = [];
+    let boardDataObj = {
+      boardName: 'No name board',
+      stages: []
+    };
+
+    // let boardData = [];
 
     let userData = document.getElementsByClassName('new_board_td');
     let boardName = document.getElementById('bord-name').value;
-    boardData.push(boardName);
+    // boardData.push(boardName);
+    if(boardName !== ''){
+        boardDataObj.boardName = boardName;
+    }
+
     for(let stage of userData){
         let cardStage = []; // 0: New, 1: InProgress, 2: Test, 3: Done
-        boardData.push(cardStage);
+        // boardData.push(cardStage);
+        boardDataObj.stages.push(cardStage);
         for(let i=0; i<stage.children.length; i++){
             if(stage.children[i].children[0].value !== ""){
             cardStage.push(stage.children[i].children[0].value);
@@ -17,6 +27,8 @@ export function getDataFromBoard() {
     }
     clearBoard();
     document.getElementById('new_board').style.display = 'none';
-    console.log(boardData);
-    return (boardData);
+    // console.log(boardData);
+    console.log(boardDataObj);
+    return (boardDataObj);
+    // return (boardData);
 }
