8819a62bbbb165387e667772911e089e099fef52
Harnet69 <harnet69@gmail.com>
2020-01-24 14:07:35 +0100

    Add to local store works correctly

--- static/js/data_handlers.js
+++ static/js/data_handlers.js
@@ -1,4 +1,5 @@
 import {clearBoard} from "./dom_manipulations.js"
+
 // save data from board to array
 export function getDataFromBoard() {
     let boardDataObj = {
@@ -21,6 +22,34 @@ export function getDataFromBoard() {
             }
         }
     }
-    console.log(boardDataObj);
+    // console.log(boardDataObj);
+    saveBoardToLS(boardDataObj);
+
     return (boardDataObj);
+}
+
+// get boards data from localstorage
+function getFromLocalStorage(name) {
+    let dataFromLS = localStorage.getItem(name);
+    return dataFromLS;
+}
+
+// save a board's data to a local storage
+function saveBoardToLS(boardDataObj) {
+    if(!localStorage.getItem('boards')){ // if we don't have boards in Local Store
+        let boardDataArr = [boardDataObj];
+        // boardDataArr.push(boardDataObj);
+        let boardDataJson = JSON.stringify(boardDataArr);
+        localStorage.setItem('boards', boardDataJson);
+    }else{
+        let newBoardArr = [];
+        let oldBoardData = JSON.parse(getFromLocalStorage('boards'));
+        for(let boar of oldBoardData){
+            newBoardArr.push(boar);
+        }
+        newBoardArr.push(boardDataObj);
+        let updatedData = JSON.stringify(newBoardArr);
+        localStorage.setItem('boards', updatedData);
+        console.log(updatedData);
+    }
 }
